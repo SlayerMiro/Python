7349de0ae874e18baa0708592e8e74a503f1ffb5
Miro <mr.m96@abv.bg>
2016-09-05 00:16:52 +0300

    .

--- FileDeleter.py
+++ FileDeleter.py
@@ -7,7 +7,8 @@ for file in os.listdir(directory):
         os.remove(file)
     elif file.endswith(".exe") or file.endswith(".zip") or file.endswith(".rar"):
         print(file)
-        print("Do you want to delete this? y/n")
+        print("Do you want to delete this? y/n/ya/na")
         answer = str(input())
-        if answer is "y":
-            os.remove(file)
+        if answer is "ya":
+            for file in os.listdir() file.endswith(".exe") is True:
+                os.remove(file)
--- TorrentDeleter.py
+++
@@ -1,8 +0,0 @@
-import os
-
-directory = os.getcwd()
-
-for file in os.listdir(directory):
-    if file.endswith(".torrent"):
-        os.remove(file)
-
