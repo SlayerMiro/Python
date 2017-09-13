6bbca4375d6af2e163d1449ce31854342b29a6cb
Miro <mr.m96@abv.bg>
2017-09-13 18:05:43 +0300

    Changed to work with streamlink instead of livestreamer

--- NLSSDownloader.py
+++ NLSSDownloader.py
@@ -7,22 +7,26 @@ import os.path
 from tkinter import *
 
 
-def download(url, name):
+def download(new_url, name):
     time = datetime.datetime.now()
+    directory = os.getcwd()
     os.system(
-        "livestreamer --twitch-oauth-token 6gko96m7voe5udkye0z1cyz7v01r1j -o D:\Livestreams\\" + name + "-" + str(time.second) + ".mp4 --hls-segment-threads 4 " + url + " best")
+        "streamlink --twitch-oauth-token=n3qqe95xrq5b17hopk73jxj91ahy8o -o " + directory + "\\" + name + "-" + str(time.second) + ".mp4 --hls-segment-threads 4 " + new_url + " best")
 
+
+		
 
 root = Tk()
-twitch = requests.get("https://api.twitch.tv/kraken/channels/northernlion/videos?limit=5&broadcasts=true", headers={"Client-ID": "jfyy89kdt4zdf64dzmg3zmvt91j6uca"})
-
+twitch = requests.get("https://api.twitch.tv/kraken/channels/northernlion/videos?limit=5&broadcasts=true", headers={'Client-ID': 'jfyy89kdt4zdf64dzmg3zmvt91j6uca', 'Authorization': 'OAuth n3qqe95xrq5b17hopk73jxj91ahy8o' ,'Accept': 'application/vnd.twitchtv.v3+json'})
 api = twitch.json()
+
 for x in range(0, 5):
     url = api['videos'][x]['url']
+    new_url = url#url[:29] + 'videos/' + url[29:]
     date = api['videos'][x]['recorded_at']
     name = "NLSS-" + date[0:10]
     title = Label(root, text=name)
-    downloadButton = Button(root, text="Download", command= lambda url=url, name=name, date=date: download(url, name))
+    downloadButton = Button(root, text="Download", command= lambda new_url=new_url, name=name, date=date: download(new_url, name))
     title.grid(row=x, column=0)
     downloadButton.grid(row=x, column=1)
 
