afa0a530a57d3e687fc6f6c5d93c9d38e7624470
Miro <mr.m96@abv.bg>
2017-10-04 19:19:20 +0300

    Basic stuff done

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -7,20 +7,20 @@ directory = os.getcwd()
 def play(file):
     os.startfile(file)
 
+fileList = []
+counter = 0
 root = Tk()
+root.title("Media Player")
 numFiles = 0
 for file in os.listdir(directory):
     if file.endswith(".mkv") or file.endswith(".mp4"):
-        numFiles += 1
-
-for x in range(0, numFiles):
-    for file in os.listdir(directory):
-        if file.endswith(".mkv") or file.endswith(".mp4"):
-            name = file
-            print(name)
-            title = Label(root, text=name)
-            playButton = Button(root, text="Play", command= lambda file=file: play(file))
-            title.grid(row=x, column=0)
-            playButton.grid(row=x, column=1)
+        fileList.append(file)
+for x in range(0, len(fileList)):
+    name = fileList[counter]
+    title = Label(root, text=name)
+    playButton = Button(root, text="Play", command=lambda name=name : play(name))
+    title.grid(row=x, column=0)
+    playButton.grid(row=x, column=1)
+    counter += 1
 
 root.mainloop()
