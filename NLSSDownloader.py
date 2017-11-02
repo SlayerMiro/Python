94d26ff5465f829bcdf9f1ab100c4efacfdc4085
Miro <mr.m96@abv.bg>
2017-11-02 20:28:32 +0200

    Made it better

--- NLSSDownloader.py
+++ NLSSDownloader.py
@@ -13,6 +13,15 @@ def download(new_url, name):
     os.system(
         "streamlink --twitch-oauth-token=n3qqe95xrq5b17hopk73jxj91ahy8o -o " + directory + "\\" + name + "-" + str(time.second) + ".mp4 --hls-segment-threads 4 " + new_url + " best")
 
+def caller(self):
+    clear(self=1)
+    VODs(self=1)
+
+def clear(self):
+    list = frame.grid_slaves()
+    for l in list:
+        l.destroy()
+
 def VODs(self):
     n = int(selector.get())
     twitch = requests.get("https://api.twitch.tv/kraken/channels/northernlion/videos?limit=" + str(n) + "&broadcasts=true", headers={'Client-ID': 'jfyy89kdt4zdf64dzmg3zmvt91j6uca', 'Authorization': 'OAuth n3qqe95xrq5b17hopk73jxj91ahy8o' ,'Accept': 'application/vnd.twitchtv.v3+json'})
@@ -22,8 +31,8 @@ def VODs(self):
         new_url = url  # url[:29] + 'videos/' + url[29:]
         date = api['videos'][x]['recorded_at']
         name = "NLSS-" + date[0:10]
-        title = Label(root, text=name)
-        downloadButton = Button(root, text="Download", command= lambda new_url=new_url, name=name, date=date: download(new_url, name))
+        title = Label(frame, text=name)
+        downloadButton = Button(frame, text="Download", command= lambda new_url=new_url, name=name, date=date: download(new_url, name))
         title.grid(row=x+1, column=0)
         downloadButton.grid(row=x+1, column=1)
 
@@ -33,7 +42,9 @@ selector = StringVar(root)
 selector.set("5")
 dropdownName = Label(root, text="Number of VODs")
 dropdownName.grid(row=0, column=0)
-w = OptionMenu(root, selector, "5", "10", command=VODs)
+w = OptionMenu(root, selector, "5", "10", command=caller)
 w.grid(row=0, column=1)
+frame = Frame(root)
+frame.grid()
 VODs(self=1)
 root.mainloop()
